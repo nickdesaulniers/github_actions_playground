cd1daca8f236e81551faa573f5714bb03e1f3c69
Nick Desaulniers <ndesaulniers@google.com>
2020-12-15 12:23:12 -0800

    get rid of initial scripts

--- bar.sh
+++
@@ -1,4 +0,0 @@
-cat output.txt
-echo "Arch is ${ARCH}"
-echo "LLVM is ${LLVM}"
-echo "LLVM_IAS is ${LLVM_IAS}"
--- baz.sh
+++
@@ -1,5 +0,0 @@
-echo "baz"
-cat output.txt
-echo "Arch is ${ARCH}"
-echo "LLVM is ${LLVM}"
-echo "LLVM_IAS is ${LLVM_IAS}"
